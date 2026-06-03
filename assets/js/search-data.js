fd141e59860deba79d209cbd79489999850c72f8
cc795 <114161295+cc795@users.noreply.github.com>
2026-06-03 15:37:42 +0000

    Deploying to gh-pages from @ cc795/cc795.github.io@5fe05cbfbc15b27242f67bbddadf0fc78ed16027 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -447,13 +447,6 @@ ninja.data = [{
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/introduction-to-machine-learning/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
